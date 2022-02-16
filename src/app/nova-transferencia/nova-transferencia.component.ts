51d2eeccb744f34bcc4122c6f6b8da645317bc70
Mauricio Santos @omausantos <mauwebjc@gmail.com>
2022-02-16 18:30:19 -0300

    [Aula 02] Criar função de transferência

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,10 +1,12 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
-
+  transferir() {
+    console.log('Nova tranferencia realizada');
+  }
 }
