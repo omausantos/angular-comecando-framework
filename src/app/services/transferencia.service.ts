00c495c3fba463c18a205441b4cc821c5be04891
Mauricio Santos @omausantos <mauwebjc@gmail.com>
2022-02-22 08:58:23 -0300

    [Aula 06] Integrando os extratos com a API

--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -1,21 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Transferencia } from '../models/transferencia.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransferenciaService {
-  private listaTransferencia: Array<any>;
+  private listaTransferencia: Array<Transferencia>;
+  private urlApiRest = 'http://localhost:3000/transferencias';
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this.listaTransferencia = [];
   }
 
-  public adicionar(transferencia: any): void {
+  public adicionar(transferencia: Transferencia): void {
     this.listaTransferencia.push({...transferencia, data: new Date()});
   }
 
-  public get transferencias(): Array<any> {
+  public get transferencias(): Array<Transferencia> {
     return this.listaTransferencia;
+  }
+
+  public todas() {
+    return this.httpClient.get<Transferencia[]>(this.urlApiRest);
   }
 
 }
